64a403259f89ae8cfcda37291aaa83796c3001f9
wys <1394945493@qq.com>
2026-09-08 22:35:09 +0800

    修复训练bug

--- tools/train.py
+++ tools/train.py
@@ -41,7 +41,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
     parser.add_argument('config', default=None, help='train config file path')
     parser.add_argument('--work-dir', default="work_dirs/baseline_0630_temporal", help='the dir to save logs and models')
-    parser.add_argument('--loadcheckpoint', default=None,
+    parser.add_argument('--load-checkpoint', default=None,
         help="the dir to load models" )
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
@@ -88,7 +88,7 @@ def parse_args():
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='job launcher')
-    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--local_rank', '--local-rank',  dest='local_rank', type=int, default=0)
     parser.add_argument(
         '--autoscale-lr',
         action='store_true',
