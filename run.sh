e499e6ee8fa24ddc5d65c4a26f352327c9b928b7
wys <1394945493@qq.com>
2026-09-09 01:10:57 +0000

    0909

--- run.sh
+++ run.sh
@@ -47,7 +47,7 @@ PYTHONPATH="$(pwd):${PYTHONPATH:-}" \
 torchrun --nproc_per_node=2 \
     tools/test.py \
     projects/configs/occupancy/semantickitti/temporal_baseline_custom.py\
-    /vepfs-mlp2/c20250502/haoce/wangyushen/Outputs/htcl/train/epoch_1.pth \
+    /c20250502/wangyushen/Outputs/htcl/htcl/semkitti/train/epoch_1.pth \
     --launcher pytorch \
     --deterministic \
     --tmpdir /vepfs-mlp2/c20250502/haoce/wangyushen/Outputs/htcl/val/.dist_test \
